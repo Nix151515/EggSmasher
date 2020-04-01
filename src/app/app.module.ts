267831c49635fa9e55f00999076dbc60e8672eea
State Nicolae <statenicolae15@gmail.com>
2020-04-01 20:11:27 +0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LobbyComponent } from './components/lobby/lobby.component';
+import { SocketsService } from './services/sockets.service';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { UsersService } from './services/users.service';
+import { GameComponent } from './components/game/game.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LobbyComponent,
+    WelcomePageComponent,
+    GameComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [SocketsService, UsersService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
